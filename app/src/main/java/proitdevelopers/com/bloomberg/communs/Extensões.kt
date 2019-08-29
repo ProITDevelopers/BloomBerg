cf1282a392f4213e3ac4acc0636087a9d3c06faa
Bacar Pereira <bacarpereira19@hotmail.com>
2019-08-29 10:08:28 +0100

    Home Com Categorias. Videos, audios em falta.

--- app/src/main/java/proitdevelopers/com/bloomberg/communs/Extensões.kt
+++ app/src/main/java/proitdevelopers/com/bloomberg/communs/Extensões.kt
@@ -1,2 +1,9 @@
-package proitdevelopers.com.bloomberg.Communs
+package proitdevelopers.com.bloomberg.communs
 
+import android.view.View.GONE
+import android.widget.ImageView
+
+fun esconderSeparador(imgSeparador:ImageView,posicao:Int,limite:Int){
+    if (posicao==limite-1)
+        imgSeparador.visibility = GONE
+}
